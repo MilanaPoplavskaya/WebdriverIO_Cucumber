92e8a71ad0f4f55d00d5b23282ba129deff1ccda
Milana <milanin7147@gmail.com>
2022-07-27 16:06:21 +0400

    some corrections in helpers.ts file

--- features/pageobjects/helpers.ts
+++ features/pageobjects/helpers.ts
@@ -1,5 +1,5 @@
 export const getWebElement = async (elementLocator: string): Promise<WebdriverIO.Element> => {
     const webElement = await $(elementLocator);
-    await webElement.waitForExist({ timeout: 500000, reverse: false });
+    await webElement.waitForExist({ timeout: 5000, reverse: false });
     return webElement;
 };
